14d37ce5d45e8df18f2c38e4b7ce222f482f4d97
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-01-04 23:06:27 +0100

    improve database

--- app/db/database.py
+++ app/db/database.py
@@ -5,36 +5,38 @@ class DeviceGroup(SQLModel, table=True):
     DG_libelle: str = Field(index=True)
 
 class Device(SQLModel, table=True):
-    D_id: int = Field(index=True, primary_key=True)
-    D_name: str = Field(index=True)
-    D_os: str = Field(index=True)
-    D_group_device_id: int | None = Field(default=None, index=True, foreign_key="devicegroup.DG_id")
+    DEV_id: int = Field(index=True, primary_key=True)
+    DEV_name: str = Field(index=True)
+    DEV_os: str = Field(index=True)
+    DG_id: int | None = Field(default=None, index=True, foreign_key="devicegroup.DG_id")
 
 class PackageGroup(SQLModel, table=True):
     PG_id: int = Field(index=True, primary_key=True)
     PG_libelle: str = Field(index=True)
 
 class Package(SQLModel, table=True):
-    P_id: int = Field(index=True, primary_key=True)
-    P_name: str = Field(index=True)
-    P_path: str = Field(index=True)
-    P_type: str = Field(index=True)
-    P_os_supported: str = Field(index=True)
-    P_for_device_id: int | None = Field(default=None, index=True, foreign_key="device.D_id")
-    P_for_group_id: int | None = Field(default=None, index=True, foreign_key="devicegroup.DG_id")
-    P_package_group_id: int = Field(default=None, index=True, foreign_key="packagegroup.PG_id")
+    PACK_id: int = Field(index=True, primary_key=True)
+    PACK_name: str = Field(index=True)
+    PACK_type: str = Field(index=True)
+    PACK_os_supported: str = Field(index=True)
+    DEV_id: int | None = Field(default=None, index=True, foreign_key="device.DEV_id")
+    DG_id: int | None = Field(default=None, index=True, foreign_key="devicegroup.DG_id")
+    PG_id: int | None = Field(default=None, index=True, foreign_key="packagegroup.PG_id")
 
+"""
 class Role(SQLModel, table=True):               
-    R_id: int = Field(default=None, index=True, primary_key=True)
-    R_libelle: str = Field(index=True)
-    R_permissions: int = Field(index=True)
+    ROL_id: int = Field(default=None, index=True, primary_key=True)
+    ROL_libelle: str = Field(index=True)
+    ROL_perms: int = Field(index=True)
+"""
 
 class User(SQLModel, table=True):
-    U_id: int = Field(index=True, primary_key=True)
-    U_username: str = Field(index=True)
-    U_passHash: str = Field(index=True)
-    U_permissions: int = Field(index=True)
-    #U_role_id: int = Field(index=True, foreign_key="role.R_id")
+    USER_id: int = Field(index=True, primary_key=True)
+    USER_username: str = Field(index=True)
+    USER_passHash: str = Field(index=True)
+    USER_permissions: int = Field(index=True)
+    USER_isActive: bool = Field(index=True)
+    #ROL_perms: int = Field(index=True, foreign_key="role.ROL_perms")
 
 
 def create_db(engine):
