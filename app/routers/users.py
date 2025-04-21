fb27016f441f5dfb7bf9f305673db32d8edef630
valentinRyckaert <v.ryckaert@eleve.leschartreux.net>
2025-04-21 16:04:43 +0200

    Fix: add pass hashing for put method in users

--- app/routers/users.py
+++ app/routers/users.py
@@ -128,7 +128,7 @@ def update_user(user_id: int, user: User, session: SessionDep, request: Request,
         })
         return HTTPException(status_code=404, detail="User not found")
     db_user.USER_username = user.USER_username
-    db_user.USER_passHash = user.USER_passHash
+    db_user.USER_passHash = get_password_hash(user.USER_passHash)
     db_user.USER_type = user.USER_type
     db_user.USER_isActive = user.USER_isActive
     session.add(db_user)
